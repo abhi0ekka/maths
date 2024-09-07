a5afa70d18a3a2c06c211c61140d7e10d7cc0f0c
WREAK <abhi0ekka@gmail.com>
2024-09-07 10:47:23 +0530

    Prime and Primarity

---
+++ prime_number.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+bool naivePrime(int n)
+{
+    if (n == 1)
+        return false;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool effectivePrime(int n)
+{
+    if (n == 1)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool moreEffectivePrime(int n)
+{
+    if (n == 1)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i++)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number \n";
+    cin >> n;
+    cout << naivePrime(n) << endl;
+    cout << effectivePrime(n) << endl;
+    cout << moreEffectivePrime(n) << endl;
+}
